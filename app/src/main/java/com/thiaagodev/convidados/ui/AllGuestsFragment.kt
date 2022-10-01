1189f2b547e2c11a44d9bf08a0b0ae53323a3273
Thiago Ferreira <thiaago.dev@gmail.com>
2022-10-01 16:59:37 -0300

    modificações iniciais

--- app/src/main/java/com/thiaagodev/convidados/ui/AllGuestsFragment.kt
+++ app/src/main/java/com/thiaagodev/convidados/ui/AllGuestsFragment.kt
@@ -1,4 +1,4 @@
-package com.thiaagodev.convidados.ui.home
+package com.thiaagodev.convidados.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.thiaagodev.convidados.databinding.FragmentHomeBinding
+import com.thiaagodev.convidados.databinding.FragmentAllGuestsBinding
 
-class HomeFragment : Fragment() {
+class AllGuestsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentAllGuestsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val allGuestsViewModel =
+            ViewModelProvider(this).get(AllGuestsViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        allGuestsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/thiaagodev/convidados/ui/PresentViewModel.kt
+++ app/src/main/java/com/thiaagodev/convidados/ui/PresentViewModel.kt
@@ -1,13 +1,13 @@
-package com.thiaagodev.convidados.ui.home
+package com.thiaagodev.convidados.ui
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class PresentViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is gallery Fragment"
     }
     val text: LiveData<String> = _text
 }
